e882d98c9d143cdea0db53f9976d164023da6607
岡野　哲真 <tetsuma@okanoakirashinnoMacBook-Pro.local>
2019-01-29 20:30:00 +0900

    make it better

---
+++ webhook.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+require_once('./LINEBotTiny.php');
+
+$channelAccessToken = '36sJhHi1aOf/hFK0rIMqstX8gKWMb8S6S2TwVMMuJIzbklvi/8NlZEguGaz5VM/LYNvNC9absNvD+Zi7dfmIuvzdu7wDkKLnRCtxT/3XU3a8wB6A/EBjvDozyNppNkL2jMNKJcmq3433NGmitluFTwdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '430f3934a9bfeabe83511321ed27d7ce';
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage([
+                        'replyToken' => $event['replyToken'],
+                        'messages' => [
+                            [
+                                'type' => 'text',
+                                'text' => $message['text']
+                            ]
+                        ]
+                    ]);
+                    break;
+                default:
+                    error_log('Unsupported message type: ' . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log('Unsupported event type: ' . $event['type']);
+            break;
+    }
+};
