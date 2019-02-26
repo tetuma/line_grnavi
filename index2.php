ed892b6e14d019e386b7ae81a76800ee0b4616a1
岡野　哲真 <tetsuma@okanoakirashinnoMacBook-Pro.local>
2019-02-26 19:55:41 +0900

    debug

--- index2.php
+++ index2.php
@@ -1,33 +1,39 @@
 <?php
 
+
+
 $accessToken = '8v7Q2Wosv/C0jdZmzylOycv4TIKF6LBFSzfHloY+MDBw+guQzbyfvJEYXSU8oaVnYNvNC9absNvD+Zi7dfmIuvzdu7wDkKLnRCtxT/3XU3Yme50fFVaFndkDng6b6/PDPUGvHysgDyZhnLQJpW0TGwdB04t89/1O/w1cDnyilFU=';
 
-// 受信したメッセージ情報
-$raw = file_get_contents('php://input');
-$receive = json_decode($raw, true);
-
-$event = $receive['events'][0];
-$replyToken  = $event['replyToken'];
-$messageType = $event['message']['type'];
+//// 受信したメッセージ情報
+//$raw = file_get_contents('php://input');
+//$receive = json_decode($raw, true);
+//
+//$event = $receive['events'][0];
+//$replyToken  = $event['replyToken'];
+//$messageType = $event['message']['type'];
 
 // 送られてきたのが位置情報以外だったら応答しない
-if($messageType != "location") exit;
+//if($messageType != "location") exit;
 
-$lat = $event['message']['latitude'];
-$lon = $event['message']['longitude'];
+//$lat = $event['message']['latitude'];
+//$lon = $event['message']['longitude'];
+$lat = 35.733709;
+$lon = 139.740354;
 
 // 送られてきた位置情報を元にぐるなびのAPIにアクセスしてケンタッキーの店舗情報を取得する
-$uri    = 'https://api.gnavi.co.jp/RestSearchAPI/v3/?fbclid=IwAR1cnbVIvDb-2YkS_R9B7DyJYq6J-A1KwDUKi0FqCx_ezVtw4R_RCEsIGPo';
+$uri    = 'https://api.gnavi.co.jp/RestSearchAPI/v3/';
+
+//$accKey = '180222565e51c56e7198ca6d82202b6b';
 $accKey = '180222565e51c56e7198ca6d82202b6b';
 
-$url  = $uri . '?format=json&name=ケンタッキー&range=5&keyid=' . $accKey . '&latitude=' . $lat . '&longitude=' . $lon;
-
+$url  = $uri . '?format=json&range=5&keyid=' . $accKey . '&latitude=' . $lat . '&longitude=' . $lon;
 $json = file_get_contents($url);
 $obj  = json_decode($json);
 
 // 店舗情報を取得
 $count = 0;
 $columns = array();
+
 foreach ($obj->rest as $restaurant) {
   $columns[] = array(
     'thumbnailImageUrl' => $restaurant->image_url->shop_image1,
